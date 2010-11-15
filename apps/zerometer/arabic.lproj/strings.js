88ef028a73e1160f9884cdbee3593ba40a07e6fc
Brad Robertson <bradleyrobertson@gmail.com>
2010-11-15 15:18:57 -0500

    cleaning up theme

--- apps/zerometer/arabic.lproj/strings.js
+++ apps/zerometer/arabic.lproj/strings.js
@@ -1,6 +1,5 @@
-// SC.stringsFor("ar", {
-//   "hello": 'مرحبا',
-//   "world": 'العالم'
-// });
+SC.stringsFor("ar", {
+  "hello": 'مرحبا',
+  "world": 'العالم'
+});
 
-// why does adding this
--- apps/zerometer/arabic/strings.js
+++
@@ -1,6 +0,0 @@
-// SC.stringsFor("ar", {
-//   "hello": 'مرحبا',
-//   "world": 'العالم'
-// });
-
-// why does adding this
